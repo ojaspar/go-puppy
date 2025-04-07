4792577a7b8598dd9763593b0dbd9a18992dc4a3
Divine Ogbonna <ojaspar@gmail.com>
2025-04-07 22:05:28 +0100

    dog package added

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/ojaspar/go-puppy
 
 go 1.24.1
+
+require github.com/ojaspar/go-dog v0.0.0-20250407205932-046479cef411 // indirect
--- puppy.go
+++ puppy.go
@@ -1,9 +1,17 @@
 package puppy
 
+import (
+	"github.com/ojaspar/go-dog"
+)
+
 func Bark() string {
 	return "woof!!"
 }
 
 func Barks() string {
 	return "woof woof!!"
+}
+
+func Name() string {
+	return dog.WhenGrowUp("Krane")
 }
